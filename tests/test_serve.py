97caf5d79119ec3352a2552af0635fd994db6bc3
Panu Tangchalermkul <panuta@gmail.com>
2018-01-18 22:44:44 +0700

    Add CORS headers to fix issue with webfonts
    For web browser to be able to load and apply webfonts to web page correctly from different domain, we need to add these Cross-origin Resource Sharing headers to static files served from devtools

--- tests/test_serve.py
+++ tests/test_serve.py
@@ -20,6 +20,7 @@ async def test_simple_serve(cli, tmpworkdir):
     r = await cli.get('/foo')
     assert r.status == 200
     assert r.headers['content-type'] == 'application/octet-stream'
+    assert r.headers['Access-Control-Allow-Origin'] == '*'
     text = await r.text()
     assert text == 'hello world'
 
