bb61601f73e370fda3fe381da351fea17f54549d
BR <32102033+bruziev@users.noreply.github.com>
2018-09-22 12:03:56 +0300

    correct import order

--- aiohttp_devtools/start/template/app/gunicorn.py
+++ aiohttp_devtools/start/template/app/gunicorn.py
@@ -9,8 +9,8 @@ To run the app using gunicorn, in the terminal run
 
 You could use a variant of the above with heroku (in the `Procfile`) or with Docker in the ENTRYPOINT statement.
 """
-from .main import create_app
 import asyncio
+from .main import create_app
 
 loop = asyncio.get_event_loop()
 
