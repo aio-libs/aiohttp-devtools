c1f7b3e9f20602d859d1a33bc6db38aebac43de7
Samuel Colvin <s@muelcolvin.com>
2018-11-18 17:43:02 +0000

    tweak _find_app_path for backwards compatibility

--- aiohttp_devtools/runserver/config.py
+++ aiohttp_devtools/runserver/config.py
@@ -73,7 +73,10 @@ class Config:
         return self.static_path and str(self.static_path)
 
     def _find_app_path(self, app_path: str) -> Path:
-        path = Path(app_path).resolve()
+        # for backwards compatibility try this first
+        path = (self.root_path / app_path).resolve()
+        if not path.exists():
+            path = Path(app_path).resolve()
         if path.is_file():
             logger.debug('app_path is a file, returning it directly')
             return path
