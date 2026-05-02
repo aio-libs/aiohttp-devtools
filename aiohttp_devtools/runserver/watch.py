128235df4f4ab53c99389820fb4ab3300edeb715
Sam Bull <git@sambull.org>
2026-05-02 03:06:54 +0100

    Apply suggestion from @Dreamsorcerer

--- aiohttp_devtools/runserver/watch.py
+++ aiohttp_devtools/runserver/watch.py
@@ -62,7 +62,7 @@ class AppTask(WatchTask):
         self._reloads = 0
         self._session: Optional[ClientSession] = None
         self._runner = None
-        self._client_ssl_context: Union[bool, SSLContext] = False
+        self._client_ssl_context: Union[bool, SSLContext] = True
         assert self._config.watch_path
 
         super().__init__(self._config.watch_path)
